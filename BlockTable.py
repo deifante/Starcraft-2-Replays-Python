b999117817ed91db1ced6bc37eb66aca37f2e72e
Deifante Walters <deifante@gmail.com>
2012-04-27 16:04:21 -0600

    Added reading of the flags into human readable format.

--- BlockTable.py
+++ BlockTable.py
@@ -4,6 +4,15 @@ from collections import namedtuple
 class BlockTable(object):
     """Reads a Block Table from a Starcraft 2 Replay."""
     BlockTableTuple = namedtuple('BlockTableTuple', 'block_offset block_size file_size flags')
+    IS_FILE            = 0x80000000
+    HAS_CHECKSUM       = 0x04000000
+    IS_DELETION_MARKER = 0x02000000
+    IS_SINGLE_UNIT     = 0x01000000
+    IS_KEY_ADJUSTED    = 0x00020000
+    IS_ENCRYPTED       = 0x00010000
+    IS_COMPRESSED      = 0x00000200
+    IS_IMPLODED        = 0x00000100
+
     def __init__(self, replay_file, archive_header, replay_reader):
         """Instantiate a block table reading object.
 
@@ -11,7 +20,7 @@ class BlockTable(object):
         replay_file -- The already opened replay file.
         archive_header -- Archive Header information from the same replay file.
         """
-        self._block_table    = None
+        self._tables         = []
         self._archive_header = archive_header
         self._replay_file    = replay_file
         self._replay_reader  = replay_reader
@@ -36,15 +45,39 @@ class BlockTable(object):
        block_table_key = self._replay_reader.hash('(block table)', 3)
        decrypted_block_table = self._replay_reader.decrypt(block_data, block_table_length, block_table_key)
 
-       # I expect this to fail because decrypted_block_table will be to long... and of an incompatible type
-       # perhaps I can read a 16 byte (4*int32s) chunk @ a time?
-       self._table = BlockTable.BlockTableTuple._make(block_table_struct.unpack_from(decrypted_block_table))
+       for i in range(self._archive_header.block_table_entries):
+           self._tables.append(BlockTable.BlockTableTuple._make(
+                   block_table_struct.unpack_from(decrypted_block_table[16*i:])))
        self._replay_file.seek(0)
 
+    def print_flags(self, flags):
+        accumulator = ''
+        if flags & BlockTable.IS_FILE:
+            accumulator += 'Is File '
+        if flags & BlockTable.HAS_CHECKSUM:
+            accumulator += 'Has Checksum '
+        if flags & BlockTable.IS_DELETION_MARKER:
+            accumulator += 'Is Deletion Marker '
+        if flags & BlockTable.IS_SINGLE_UNIT:
+            accumulator += 'Is Single Unit '
+        if flags & BlockTable.IS_KEY_ADJUSTED:
+            accumulator += 'Key is Adjusted '
+        if flags & BlockTable.IS_ENCRYPTED:
+            accumulator += 'Is Encrypted '
+        if flags & BlockTable.IS_COMPRESSED:
+            accumulator += 'Is Compressed '
+        if flags & BlockTable.IS_IMPLODED:
+            accumulator += 'Is Imploded '
+        return accumulator
+
     def __str__(self):
-        return '''MPQ Block Table
+        accumulator = ''
+        for table in self._tables:
+            accumulator += '''MPQ Block Table
    Block Offset :{0:9}
    Block Size   :{1:9}
    File Size    :{2:9}
-   Flags        :{3:9}
-'''.format(self._table.block_offset, self._table.block_size, self._table.file_size, self._table.flags)
+   Flags        :{3:9} ({4})
+
+'''.format(table.block_offset, table.block_size, table.file_size, table.flags, self.print_flags(table.flags))
+        return accumulator
